bbfdc0a62eab669eb9b9305ef7d74b3cfca05cfa
Paul Ticona <paulticona264riddick@gmail.com>
2022-06-21 00:39:51 -0500

    Actualizando propiedades flex a la lista Y iniciando semana4 JavaScript

---
+++ semana4/js/index.js
@@ -0,0 +1,10 @@
+const n1= prompt("ingrese lado 1: ")
+const n2= prompt("ingrese lado 2: ")
+const n3= prompt("Ingrese Radio: ")
+
+const areaRect= n1 * n2
+const areaCirculo= Math.PI*Math.pow(n3,2)
+alert("El Area de una rectangulos es: "+ areaRect,
+
+)
+alert("el Area de un cirulo es: " + areaCirculo )
