21446b7bf2e2ae30fb6355fefbb47369ef084734
Paul Ticona <paulticona264riddick@gmail.com>
2022-06-30 08:31:11 -0500

    Creando data del proyecto storeNFT

---
+++ semana5/ProyectoNFT/js/nft.js
@@ -0,0 +1,42 @@
+const artNFTs = [
+    {
+        imagen:
+        "https://img.seadn.io/files/dbbe1461c731ec219482ff02dc7b3511.png?fit=max-",
+        autor: "Sudfah de Melissa wiederrecht",
+        seudonimo: "Sudfah",
+        dueños: 0,
+        precio_minimo: 1.7,
+        precio: 2,
+        tipo: "coleccionable",
+        token_id: "328000380",
+        blockchain: "Etherium",
+        contract_address: "0xa1"
+    },
+    {
+        imagen:
+        "https://img.seadn.io/files/6cc2223e65552af70f2757d924408b63.png?fit=max",
+        autor: "Sudfah de Melissa wiederrecht",
+        seudonimo: "Sudfah",
+        dueños: 1,
+        precio_minimo: 1.7,
+        precio: 2,
+        tipo: "coleccionable",
+        token_id: "328000296",
+        blockchain: "Etherium",
+        contract_address: "0xa2",
+        descripcion: "Por melissawiederrecht Sudfah ('feliz accidente' en árabe) es una colección generativa que celebra la belleza que puede surgir del caos, los errores y los accidentes. Una sola línea caligráfica pretende contar una historia (a veces errante y confusa por derecho propio, pero siempre serpenteando de un lado a otro) El líquido digital se derrama sobre él y lleva la tinta en direcciones inesperadas, descontroladas y cuenta una historia mucho más interesante que la que pretendía la línea... y muchas veces más hermosa."
+    },
+    {
+        imagen:
+        "https://img.seadn.io/files/dbbe1461c731ec219482ff02dc7b3511.png?fit=max-",
+        autor: "Sudfah de Melissa wiederrecht",
+        seudonimo: "Sudfah",
+        dueños: 0,
+        precio_minimo: 1.7,
+        precio: 2.3,
+        tipo: "coleccionable",
+        token_id: "328000380",
+        blockchain: "Etherium",
+        contract_address: "0xa3"
+    }
+]
--- semana5/js/datos.js
+++ semana5/js/datos.js
@@ -1,98 +1,98 @@
 const laptops = [
-  {
-    marca: "Lenovo",
-    imagen:
-      "http://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2021/05/05131939/0013.jpg",
-    nombre: "Ide Centre AIO I3",
-    vendedor: "Falabella",
-    precioOferta: 1599,
-    precioNormal: 3599,
-    calificacion: 2,
-    caracteristicas: {
-      procesador: "i3",
-      tarjetaDeVideo: "integrada",
-      discoDuro: true,
-      discoDuroSolido: "1TB",
+    {
+      marca: "Lenovo",
+      imagen:
+        "https://lh3.googleusercontent.com/zQrdokZwHbZDA4Q6yP5yRvvj-wOwfYx1BPnpO6ktfHz0YT0sYCE4LTYdAlloXvZAHK4J2Kr6bFYz4gk6ecWVpdb1aVIRsRQwn982aQ=w600",
+      nombre: "Ide Centre AIO I3",
+      vendedor: "Falabella",
+      precioOferta: 1599,
+      precioNormal: 3599,
+      calificacion: 2,
+      caracteristicas: {
+        procesador: "i3",
+        tarjetaDeVideo: "integrada",
+        discoDuro: true,
+        discoDuroSolido: "1TB",
+      },
     },
-  },
-  {
-    marca: "Acer",
-    imagen:
-      "http://static.acer.com/up/Resource/Acer/Laptops/Aspire_5/images/20220427/acer-aspire-5-a514-55-55g-fingerprint-backlit-steel-gray-modelpreview.png",
-    nombre: "AN515 15.6",
-    vendedor: "Falabella",
-    precioOferta: 3399,
-    precioNormal: 4999,
-    calificacion: 5,
-    caracteristicas: {
-      procesador: "i5",
-      tarjetaDeVideo: "GTX 1650",
-      discoDuro: true,
-      discoDuroSolido: "256gb",
+    {
+      marca: "Acer",
+      imagen:
+        "https://lh3.googleusercontent.com/3FBcDGQ2C6lyx21U7PHux065r_8Vfakip5xua2lC7636_VqFRwA7MAZyyjyP62aZBG3HFRVTQAc2z_REZxTeGC6wip9Iqgmamqgx=w600",
+      nombre: "AN515 15.6",
+      vendedor: "Falabella",
+      precioOferta: 3399,
+      precioNormal: 4999,
+      calificacion: 5,
+      caracteristicas: {
+        procesador: "i5",
+        tarjetaDeVideo: "GTX 1650",
+        discoDuro: true,
+        discoDuroSolido: "256gb",
+      },
     },
-  },
-  {
-    marca: "HP",
-    imagen:
-      "https://static.acer.com/up/Resource/Acer/Laptops/Nitro_5/Gallery/20200413/Acer-Nitro-5_AN515-55_gallery_01.png",
-    nombre: "HP",
-    vendedor: "Falabella",
-    precioOferta: 3399,
-    precioNormal: 4999,
-    calificacion: 5,
-    caracteristicas: {
-      procesador: "i3",
-      tarjetaDeVideo: "GTX 1650",
-      discoDuro: false,
-      discoDuroSolido: "256gb",
+    {
+      marca: "HP",
+      imagen:
+        "https://lh3.googleusercontent.com/VHFrEDk7Q0TZUJ1U7GYc3Aea2bgYtt_MlUZBYLKoKLUtH8NNMnaaUn2MsXHV5UdAARz4TNZk-V-LWoJ9s3suMCr7_g=w600",
+      nombre: "HP",
+      vendedor: "Falabella",
+      precioOferta: 3399,
+      precioNormal: 4999,
+      calificacion: 5,
+      caracteristicas: {
+        procesador: "i3",
+        tarjetaDeVideo: "GTX 1650",
+        discoDuro: false,
+        discoDuroSolido: "256gb",
+      },
     },
-  },
-  {
-    marca: "apple",
-    imagen:
-      "https://www.apple.com/v/macbook-pro-14-and-16/b/images/overview/hero/intro__ewz1ro7xs14y_large.jpg",
-    nombre: "MacBook Pro",
-    vendedor: "Falabella",
-    precioOferta: 8399,
-    precioNormal: 4999,
-    calificacion: 5,
-    caracteristicas: {
-      procesador: "M1 Pro",
-      tarjetaDeVideo: "GTX 1650",
-      discoDuro: true,
-      discoDuroSolido: "256gb",
+    {
+      marca: "apple",
+      imagen:
+        "https://lh3.googleusercontent.com/M_RQU1gNeABhCZ_y75DJg4W6H9cbPomySl0MMTrk_Q6bJDNbQneeUVFMQksNXtdwWbGadipccQr4AuaORxRPbABXA3QTibnY-KkSlQ=w600",
+      nombre: "MacBook Pro",
+      vendedor: "Falabella",
+      precioOferta: 8399,
+      precioNormal: 4999,
+      calificacion: 5,
+      caracteristicas: {
+        procesador: "M1 Pro",
+        tarjetaDeVideo: "GTX 1650",
+        discoDuro: true,
+        discoDuroSolido: "256gb",
+      },
     },
-  },
-  {
-    marca: "Lenovo",
-    imagen:
-      "http://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2021/05/05131939/0013.jpg",
-    nombre: "Ide Centre AIO I3",
-    vendedor: "Falabella",
-    precioOferta: 1599,
-    precioNormal: 3599,
-    calificacion: 2,
-    caracteristicas: {
-      procesador: "i3",
-      tarjetaDeVideo: "integrada",
-      discoDuro: true,
-      discoDuroSolido: "1TB",
+    {
+      marca: "Lenovo",
+      imagen:
+        "https://img.seadn.io/files/534e5289095a4e1958724e1ae07e7083.png?fit=max&w=600",
+      nombre: "Ide Centre AIO I3",
+      vendedor: "Falabella",
+      precioOferta: 1599,
+      precioNormal: 3599,
+      calificacion: 2,
+      caracteristicas: {
+        procesador: "i3",
+        tarjetaDeVideo: "integrada",
+        discoDuro: true,
+        discoDuroSolido: "1TB",
+      },
     },
-  },
-  {
-    marca: "HP ",
-    imagen:
-      "https://static.acer.com/up/Resource/Acer/Laptops/Nitro_5/Gallery/20200413/Acer-Nitro-5_AN515-55_gallery_01.png",
-    nombre: "HP Laptop Gamer",
-    vendedor: "Falabella",
-    precioOferta: 3399,
-    precioNormal: 4999,
-    calificacion: 5,
-    caracteristicas: {
-      procesador: "i3",
-      tarjetaDeVideo: "GTX 1650",
-      discoDuro: false,
-      discoDuroSolido: "256gb",
+    {
+      marca: "HP ",
+      imagen:
+        "https://lh3.googleusercontent.com/w885allo52as5IhQWrhZIV1gy0U1B-XWxBXmlv3u_arJatY16Ub4YDic3haI6qKeqFijTu4YRcv-FuL_bot6HI0p7mJcRBGuBw3A=w600",
+      nombre: "HP Laptop Gamer",
+      vendedor: "Falabella",
+      precioOferta: 3399,
+      precioNormal: 4999,
+      calificacion: 5,
+      caracteristicas: {
+        procesador: "i3",
+        tarjetaDeVideo: "GTX 1650",
+        discoDuro: false,
+        discoDuroSolido: "256gb",
+      },
     },
-  },
-];
+  ];
